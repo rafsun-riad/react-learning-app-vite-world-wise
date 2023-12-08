bcb68e53f64cc07e5e216e3ef00839fb096582d8
rafsun-riad <mruhaquer@gmail.com>
2023-12-09 02:00:42 +0600

    cleaned the app

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,6 @@ import { CitiesProvider } from './contexts/CitiesContext';
 import { AuthProvider } from './contexts/FakeAuthContext';
 import ProtectedRoute from './pages/ProtectedRoute';
 
-// import Product from './pages/Product';
-// import Pricing from './pages/Pricing';
-// import Homepage from './pages/Homepage';
-// import PageNotFound from './pages/PageNotFound';
-// import AppLayout from './pages/AppLayout';
-// import Login from './pages/Login';
-
 import CityList from './components/CityList';
 import CountryList from './components/CountryList';
 import City from './components/City';
